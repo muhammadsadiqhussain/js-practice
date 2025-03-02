e1b2a72c89df4ae85f9014db20241d6df100075e
muhammadsadiqhussain <sh2453621@gmail.com>
2025-03-02 16:34:42 +0500

    js-practice

---
+++ practice.js
@@ -0,0 +1,87 @@
+// form practice
+
+// let name = "";
+// let email = "";
+// let password = "";
+// let contact = "";
+// let gender = "";
+
+// alert("Welcome");
+
+// alert("Enter Your Username");
+// name = prompt("please enter your name");
+
+// alert("Enter Your email");
+// email = prompt("please enter your email");
+
+// alert("Enter Your password");
+// password = prompt("please enter your password");
+
+// alert("Enter Your contact number");
+// contact = prompt("please enter your contact number");
+
+// alert("Enter Your gender");
+// gender = prompt("please enter your gender");
+
+// console.table([name, email, password, contact, gender])
+
+
+// math practice
+
+// Q1
+
+let x = 25;
+let y = 5;
+let z = 2;
+
+let result;
+
+result = (x / y) + (y * z);
+console.log(result);
+
+// Q2
+
+let a = 50;
+let b = 80;
+let c = 10;
+
+let percent;
+
+percent = (a / b) * 100;
+percent = percent + (c / a) * 100;
+console.log(percent);
+
+// Q3
+
+let p = 12;
+let q = 4;
+let r = 8;
+
+let answer;
+
+answer = (p * r) / (q + r); // 8
+answer = answer + q / (p * r);
+console.log(answer);
+
+// Q4
+
+let num1 = 17;
+let num2 = 5;
+
+let modResult;
+
+modResult = num1 % num2;
+console.log(modResult);
+
+// Q5
+
+// Increment & Decrement
+
+let count = 10;
+
+count++;
+count--;
+count +=5;
+count -=2;
+
+console.log(count);
